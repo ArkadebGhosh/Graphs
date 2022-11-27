3f38c45c0b93bb7b6815ec2644ef187670d60f05
ArkadebGhosh <52507380+ArkadebGhosh@users.noreply.github.com>
2022-11-27 18:36:28 +0530

    Create Cycle_Directed.cpp

---
+++ Cycle_Directed.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+  
+  bool dfs(int v,  vector<int> adj[], vector<int> &visited, vector<int>&path) {
+      visited[v] = 1;
+      path[v] = 1;
+      for(auto &neigh: adj[v]) {
+          if(path[neigh] == 1)
+          return true;
+          else if(!visited[neigh] && dfs(neigh, adj, visited, path))
+          return true;
+      }
+      path[v] = 0;
+      return false;
+  }
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        // code here
+        vector<int> visited(V, 0);
+        vector<int> path(V, 0);
+        for(int i = 0; i<V; i++) {
+            if(!visited[i] && dfs(i, adj, visited, path))
+            return true;
+        }
+        return false;
+    }
+};
